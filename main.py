fd730a89e911ff95d633589f4d703b82ecb951b7
ownerofworld <43425662+ownerofworld@users.noreply.github.com>
2020-05-26 22:33:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
 
-bashh = os.system('bash')
-print(bashh)
+bashh = os.system('mkdir downloads')
+changing_dir = os.system('cd downloads')
+downloading = os.system('wget https://file-examples.com/wp-content/uploads/2017/02/zip_2MB.zip')
+print(downloading)
+list_dir = os.system('ls')
+print(list_dir)
